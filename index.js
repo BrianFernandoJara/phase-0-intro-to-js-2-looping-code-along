7c930c27eed06f4fa60a4af38df2239ea324151b
BrianFernandoJara <brianfernandojara@gmail.com>
2023-08-10 18:38:50 -0400

    Looping Lab complete

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solutions in this file
+/*
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts){
+    for (let i = 0; i < gifts.length; i++)
+    {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+    return gifts;
+}
+*/
+
+function writeCards(listOfNames, event){
+    const message = [];
+    for (let i = 0; i < listOfNames.length; i++){
+        message.push(`Thank you, ${listOfNames[i]}, for the wonderful ${event} gift!`);
+    }
+    return message;
+}
+
+function countDown(positiveNumber){
+    while(positiveNumber >= 0){
+        console.log(positiveNumber)
+        positiveNumber --;
+    }
+}
